e08233cfaddcb096bf5df1544bc763d64b10f710
AlexHazen <ahazen@uwo.ca>
2025-03-08 10:14:46 -0500

    Updated interface and added a new image

--- ParkingProb.py
+++ ParkingProb.py
@@ -5,8 +5,8 @@ from PIL import Image
 
 # This dictionary stores the dimensions for the displayed image
 DISPLAY_IMAGE_SIZE = {
-    'max_width': 300.0,
-    'max_height': 300.0
+    'max_width': 500.0,
+    'max_height': 500.0
 }
 
 # This function resizes an image that is bigger than either the max width or height to fit within them
@@ -60,26 +60,18 @@ def update_text_box(updated_text):
 
 
 # Create the application
-app = App(title='Amazing Image Identifier')
+app = App(title='Parking Problem', width=500, height=350)
 
-# Create an invisible box to hold the title — useful for setting up complex layouts
-title_box = Box(app, width='fill', align='top')
+container = Box(app, layout='grid')
 
-# Create the title and centre it in the application
-title = Text(title_box, text="Amazing Image Identifier")
-
-# Create an invisible box to hold the image and article text
-content_box = Box(app, width='fill', align='top')
 # Create an invisible box to hold the image
-picture_box = Box(content_box, align='left')
+picture_box = Box(container, grid=[0, 0])
 # Create the image and set it to the start.jpg file included in this directory
 display_image = Picture(picture_box, image=generate_display_picture(
     'start.jpg', DISPLAY_IMAGE_SIZE))
 # Create the text box for article text
-wiki_text = TextBox(content_box, multiline=True, enabled=False, scrollbar=True,
-                    text="Please choose an image to identify.", width='fill', height='fill')
-# Create a box to hold the button
-button_box = Box(app, width='fill', align='bottom')
+#wiki_text = TextBox(content_box, multiline=True, enabled=False, scrollbar=True,
+                    #text="Please choose an image to identify.", width='fill', height='fill')
 
 ### End user interface code ###
 
@@ -91,7 +83,9 @@ def update_picture():
 
 # This line creates a button with the text 'Select picture' and tells it to run the update_picture function when clicked
 # It has to come later than the other interface code, as it needs the update_picture function to be created first
-button = PushButton(button_box, text='Select picture', command=update_picture)
+button = PushButton(container, text='Select picture', command=update_picture,grid=[0, 0],width=10)
+
+button.tk.place(x=180, y=300) # adjusting the location of the button
 
 # Display the whole interface — this has to come after the button, the last piece of the interface, is created
 app.display()
